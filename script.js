032b6b0a8405bfec99dfae55893bf60d7354dd66
Pranali1811 <pranalijadhav9583@gmail.com>
2024-09-02 01:28:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const imgs = document.querySelectorAll('.header-slider ul img');
+const prev_btn = document.querySelector('.control_prev');
+const next_btn = document.querySelector('.control_next');
+
+let n = 0;
+
+function changeSlide() {
+  imgs.forEach((img, index) => {
+    img.style.display = index === n ? 'block' : 'none';
+  });
+}
+
+changeSlide();
+
+prev_btn.addEventListener('click', (e) => {
+  if (n > 0) {
+    n--;
+  } else {
+    n = imgs.length - 1;
+  }
+  changeSlide();
+});
+
+next_btn.addEventListener('click', (e) => {
+  if (n < imgs.length - 1) {
+    n++;
+  } else {
+    n = 0;
+  }
+  changeSlide();
+});
+
+const scrollContainers = document.querySelectorAll('.products');
+
+scrollContainers.forEach((scrollContainer) => {
+  scrollContainer.addEventListener('wheel', (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+  });
+});
